b7ab2e65e079009c26f388229d09b89f9026b059
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-07-02 14:11:16 +0300

    Initial commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Philipp Lazarev on 02.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
